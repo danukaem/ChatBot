39e755b0b09dd3cc2c46420f665d3f1554e28baf
danukaem <danukaem@gmail.com>
2021-03-19 00:01:12 +0530

    remove comments

--- src/main/java/com/sliit/chatApplication/model/OrderDetailsDTO.java
+++ src/main/java/com/sliit/chatApplication/model/OrderDetailsDTO.java
@@ -10,10 +10,7 @@ import lombok.Setter;
 import java.util.Date;
 import java.util.List;
 
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
+
 public class OrderDetailsDTO extends SuperDTO {
 
     private long orderId;
